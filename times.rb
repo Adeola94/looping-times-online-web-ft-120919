1cb6c6410d7fbc61a94fa20d21e8ed0523782e62
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-12-19 04:37:56 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,7 +1,10 @@
-def using_times 
+def using_times = 70
 	7.times do
 	  puts "Wingardium Leviosa' 7 times"
+	  using_times = using_times - 10
+	  puts "Leviosa #{using_times} times"
 end
+end 
 
 
 
